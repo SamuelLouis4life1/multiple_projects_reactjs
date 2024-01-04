bb4425a7a4f121fa9200f967b156841c393f9d6c
Samuel Sampeur Louis <samuel.louis@telecall.com>
2024-01-04 12:29:55 -0300

    Translate

--- src/components/layouts/header/index.jsx
+++ src/components/layouts/header/index.jsx
@@ -8,122 +8,139 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../../context/AuthContext";
 import { USER_ROLES } from "../../../utils/userRoles";
 import styles from "./index.module.css";
+import { withTranslation } from "react-i18next";
+import i18nInit from "../../../i18n"
 
-export default function Header() {
-    const [scrollY, setScrollY] = useState(0);
-    const [ borderStyle, setBorderStyle ] = useState("none");
-  
-    const { t } = useTranslation('header');
+export function Header(props) {
+	const [scrollY, setScrollY] = useState(0);
+	const [borderStyle, setBorderStyle] = useState("none");
 
-    const { user, signOut } = useContext(AuthContext);
+	const [value, setValue] = useState(props.language || props.i18n.language);
 
-    function handleScroll() {
-        setScrollY(window.scrollY);
-    }
+	useEffect(() => {
+		console.log('--->' + props.language);
+	}, [props.language]);
 
-    useEffect(() => {
-        function watchScroll() {
-          window.addEventListener("scroll", handleScroll);
-        }
-        watchScroll();
-        return () => {
-          window.removeEventListener("scroll", handleScroll);
-        };
-      });
+	const onLanguageHandle = (lang) => {
+		console.log(lang);
+		setValue(lang);
+		props.i18n.changeLanguage(lang);
+	};
+	const { t } = props;
 
-    useEffect(() => {
-        if (scrollY > 30) {
-            setBorderStyle("1px solid var(--tcColor1)");
-        } else {
-            setBorderStyle("none");
-        }
-    }, [scrollY, setBorderStyle]);
+	const { user, signOut } = useContext(AuthContext);
+
+	function handleScroll() {
+		setScrollY(window.scrollY);
+	}
+
+	useEffect(() => {
+		function watchScroll() {
+			window.addEventListener("scroll", handleScroll);
+		}
+		watchScroll();
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	});
+
+	useEffect(() => {
+		if (scrollY > 30) {
+			setBorderStyle("1px solid var(--tcColor1)");
+		} else {
+			setBorderStyle("none");
+		}
+	}, [scrollY, setBorderStyle]);
 
 
-    const activeUrl = "#";
-    const loginUrl = "/auth/signIn";
-    const logoutRedirect = "/";
-    const accountUrl = user?.roles.some(role => [USER_ROLES.ADMIN, USER_ROLES.SELLER].includes(role)) ? "/admin/orders" :  "/account";
+	const activeUrl = "#";
+	const loginUrl = "/auth/signIn";
+	const logoutRedirect = "/";
+	const accountUrl = user?.roles.some(role => [USER_ROLES.ADMIN, USER_ROLES.SELLER].includes(role)) ? "/admin/orders" :  "/account";
 
-    return (
-			<>
-				<div className={styles.headerTop}>
-					<Container fluid="xxl" className={styles.headerTopCointainer}>
-						<div>
-							<Link to={activeUrl}>
-								{t("Activate")} <BsPhone />
-							</Link>
-							{user ? (
-								<span>
-									<Link to={accountUrl}>{user.name ? user.name : user.userName}</Link>,{" "}
-									<Link to={logoutRedirect} onClick={signOut} style={{ marginLeft: 0 }}>
-										{t("signOut")} <GoSignOut />
-									</Link>
-								</span>
-							) : (
-								<Link to={loginUrl}>
-									{t("Log In")} <RiUserShared2Line />
-								</Link>
-							)}
-						</div>
-					</Container>
-				</div>
-				<Navbar
-					expand="lg"
-					sticky="top"
-					className={styles.headerMain}
-					style={{ padding: "1.5rem 0", borderBottom: borderStyle }}
-				>
-					<Container>
-						<Navbar.Toggle aria-controls="basic-navbar-nav" />
-						<Navbar.Brand className={styles.logo} href="/">
-							<img src="/imgs/Multiple Projects-logo.svg" alt="Multiple Projects" />
-						</Navbar.Brand>
-						<Navbar.Collapse
-							className={styles.collapseArea}
-							id="basic-navbar-nav"
-						>
-							<Nav className={styles.navCenter}>
-								<Nav.Link href="#home">{t("Mobility")}</Nav.Link>
-								<Nav.Link href="#link">{t("Fixed Phone")}</Nav.Link>
-								<Nav.Link href="#link">{t("Other Solutions")}</Nav.Link>
-							</Nav>
-							<Nav>
-								<NavDropdown
-									title={t("Help")}
-									id="basic-nav-dropdown"
-								>
-									<NavDropdown.Item href="#action/3.1">
-										{t("dropdownHelp.item1")}
-									</NavDropdown.Item>
-								</NavDropdown>
-							</Nav>
-							<Nav className={styles.navTopHiddenLinks}>
-								<NavDropdown.Divider />
-								<Nav.Link to={activeUrl}>
-									{t("headerTop.active")} <BsPhone />
-								</Nav.Link>
-								{user ? (
-                                    <>
-                                        <Nav.Link href={accountUrl}>
-                                            {user.name} <RiUserLine />
-                                        </Nav.Link>
-                                        <Nav.Link href={logoutRedirect} onClick={signOut}>
-                                            {t("signOut")} <GoSignOut />
-                                        </Nav.Link>
-                                    </>
-								) : (
-									<Nav.Link to={loginUrl}>
-										{t("Log In")} <RiUserShared2Line />
-									</Nav.Link>
-								)}
-							</Nav>
-						</Navbar.Collapse>
-						<Link to="cart" className={styles.linkIcon}>
-							<BsCart3 size="1.5rem" />
+	return (
+		<>
+			<div className={styles.headerTop}>
+				<Container fluid="xxl" className={styles.headerTopCointainer}>
+					<div>
+						<Link to={activeUrl}>
+							{t("Activate")} <BsPhone />
 						</Link>
-					</Container>
-				</Navbar>
-			</>
-		);
+						{user ? (
+							<span>
+								<Link to={accountUrl}>{user.name ? user.name : user.userName}</Link>,{" "}
+								<Link to={logoutRedirect} onClick={signOut} style={{ marginLeft: 0 }}>
+									{t("signOut")} <GoSignOut />
+								</Link>
+							</span>
+						) : (
+							<Link to={loginUrl}>
+								{t("Log In")} <RiUserShared2Line />
+							</Link>
+						)}
+					</div>
+				</Container>
+			</div>
+			<Navbar
+				expand="lg"
+				sticky="top"
+				className={styles.headerMain}
+				style={{ padding: "1.5rem 0", borderBottom: borderStyle }}
+			>
+				<Container>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Brand className={styles.logo} href="/">
+						<img src="/imgs/Multiple Projects-logo.svg" alt="Multiple Projects" />
+					</Navbar.Brand>
+					<Navbar.Collapse
+						className={styles.collapseArea}
+						id="basic-navbar-nav"
+					>
+						<Nav className={styles.navCenter}>
+							<Nav.Link href="#home">{t("Mobility")}</Nav.Link>
+							<Nav.Link href="#link">{t("title")}</Nav.Link>
+							<Nav.Link href="#link">{t("Other Solutions")}</Nav.Link>
+						</Nav>
+						<Nav>
+							<NavDropdown
+								title={t("Help")}
+								id="basic-nav-dropdown"
+							>
+								<NavDropdown.Item href="#action/3.1" onClick={() => onLanguageHandle('pt-BR')}>
+									{t("languagePt")}
+								</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.1" onClick={() => onLanguageHandle('en-US')} >
+									{t("languageEn")}
+								</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+						<Nav className={styles.navTopHiddenLinks}>
+							<NavDropdown.Divider />
+							<Nav.Link to={activeUrl}>
+								{t("headerTop.active")} <BsPhone />
+							</Nav.Link>
+							{user ? (
+								<>
+									<Nav.Link href={accountUrl}>
+										{user.name} <RiUserLine />
+									</Nav.Link>
+									<Nav.Link href={logoutRedirect} onClick={signOut}>
+										{t("signOut")} <GoSignOut />
+									</Nav.Link>
+								</>
+							) : (
+								<Nav.Link to={loginUrl}>
+									{t("Log In")} <RiUserShared2Line />
+								</Nav.Link>
+							)}
+						</Nav>
+					</Navbar.Collapse>
+					<Link to="cart" className={styles.linkIcon}>
+						<BsCart3 size="1.5rem" />
+					</Link>
+				</Container>
+			</Navbar>
+		</>
+	);
 }
+export default withTranslation()(Header)
--- src/i18n.js
+++ src/i18n.js
@@ -1,32 +1,39 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-// don't want to use this?
-// have a look at the Quick start guide 
-// for passing in lng and translations on init
+import i18n from 'i18next'
+import LanguageDetector from "i18next-browser-languagedetector"
+import {initReactI18next} from 'react-i18next'
+import XHR from 'i18next-xhr-backend'
+import languageEN from './locate/en/translate.json'
+import languagePTBR from './locate/pt-br/translate.json'
+import backend from "i18next-xhr-backend";
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
-  .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    fallbackLng: 'pt', // TODO: change to 'en' when the app is ready for internationalization
+.use(LanguageDetector)
+.use(XHR)
+.use(initReactI18next)
+.init({
+    resources: {
+        en: languageEN,
+        ptbr: languagePTBR
+    },
+    /* default language when load the website in browser */
+    lng: "ptbr",
+    /* When react i18next not finding any language to as default in borwser */
+    fallbackLng: "ptbr",
+    /* debugger For Development environment */
     debug: true,
-
+    ns: ["translations"],
+    defaultNS: "translations",
+    keySeparator: ".",
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+        escapeValue: false,
+        formatSeparator: ","
+    },
+    react: {
+        wait: true,
+        bindI18n: 'languageChanged loaded',
+        bindStore: 'added removed',
+        nsMode: 'default'
     }
-  });
-
+})
 
 export default i18n;
